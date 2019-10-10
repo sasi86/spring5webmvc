fec45ed3cf108dfa86601e70167074e1acff960e
KUMAR-P Sasi <sasi.kumar-p@rntbci.com>
2019-10-10 12:28:31 +0530

    Initial Commit
    Committer: KUMAR-P Sasi <sasi.sirius@gmail.com>

---
+++ src/main/java/com/nerd/java/spring5webmvc/Spring5webmvcApplication.java
@@ -0,0 +1,13 @@
+package com.nerd.java.spring5webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerd/java/spring5webmvc/Spring5webmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nerd.java.spring5webmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5webmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
